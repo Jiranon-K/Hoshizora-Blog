bd0d233398f7e8af095e9767566b76a2f1d94a5a
Jiranon-K <132673259+Jiranon-K@users.noreply.github.com>
2025-04-08 00:39:21 +0700

    add 'use client'on login page

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,12 +1,7 @@
+'use client';
+
 import React from 'react';
 import LoginForm from '../components/LoginForm';
-
-
-export const metadata = {
-  title: 'Hoshizora | Admin',
-  description: 'Admin Login Page',
-};
-
 
 const LoginPage = () => {
   return <LoginForm />;
