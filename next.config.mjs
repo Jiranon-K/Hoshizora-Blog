766f23b26464598029d0e2f395e0729eca5b35f7
Jiranon-K <jiranon46@gmail.com>
2026-01-20 00:17:48 +0700

    feat: Update image domains and remote patterns from `hoshizora.online` to `yuhari.app`.

--- next.config.mjs
+++ next.config.mjs
@@ -3,13 +3,24 @@ const nextConfig = {
   output: "standalone",
   images: {
     domains: [
-      "blog.hoshizora.online",
+      "blog.yuhari.app",
       "pub-2aeb1c6581944036b080b925243768cb.r2.dev",
+      "r2.yuhari.app",
     ],
     remotePatterns: [
       {
         protocol: "https",
         hostname: "pub-2aeb1c6581944036b080b925243768cb.r2.dev",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "blog.yuhari.app",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "r2.yuhari.app",
         pathname: "/**",
       },
     ],
