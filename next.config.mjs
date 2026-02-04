a4d216f03df2d9dd15bc6b74b2e8561bd8044f8c
Jiranon-K <jiranon46@gmail.com>
2026-02-05 03:31:39 +0700

    feat: Add `sanitize-html` dependency and update Next.js image configuration by removing deprecated `domains`.

--- next.config.mjs
+++ next.config.mjs
@@ -2,11 +2,6 @@
 const nextConfig = {
   output: "standalone",
   images: {
-    domains: [
-      "blog.yuhari.app",
-      "pub-2aeb1c6581944036b080b925243768cb.r2.dev",
-      "r2.yuhari.app",
-    ],
     remotePatterns: [
       {
         protocol: "https",
